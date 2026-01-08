e6034ec2ee27b00ae29bef03bb0c124f2deedb3e
Pepijn Kummel <pepijn.kummel@bumastemra.nl>
2026-01-08 10:02:11 +0000

    bugfixes and optimalisaties for running DBT

--- dbt/models/silver/rd/rd_motorconfiguratie.sql
+++ dbt/models/silver/rd/rd_motorconfiguratie.sql
@@ -3,25 +3,42 @@
     on_schema_change='sync_all_columns'
 ) }}
 
-with enriched as (
-    select distinct
-        md5(
-            concat_ws(
-                '|',
-                coalesce(aantal_cilinders, ''),
-                coalesce(cilinderinhoud, ''),
-                coalesce(type_gasinstallatie, ''),
-                coalesce(zuinigheidsclassificatie, ''),
-                coalesce(vermogen_massarijklaar, '')
-            )
-        ) as motorconfiguratie_id,
+with base as (
+    select
+        coalesce(aantal_cilinders, '')          as aantal_cilinders,
+        coalesce(cilinderinhoud, '')            as cilinderinhoud,
+        coalesce(type_gasinstallatie, '')       as type_gasinstallatie,
+        coalesce(zuinigheidsclassificatie, '')  as zuinigheidsclassificatie,
+        coalesce(vermogen_massarijklaar, '')    as vermogen_massarijklaar,
+        afwijkende_maximum_snelheid
+    from {{ source('lakehouse_bronze', 'gekentekendevoertuigen') }}
+),
+
+dedup as (
+    select
+        md5(concat_ws(
+            '|',
+            aantal_cilinders,
+            cilinderinhoud,
+            type_gasinstallatie,
+            zuinigheidsclassificatie,
+            vermogen_massarijklaar
+        )) as motorconfiguratie_id,
+
         aantal_cilinders,
         cilinderinhoud,
         type_gasinstallatie,
         zuinigheidsclassificatie,
         vermogen_massarijklaar,
-        afwijkende_maximum_snelheid
-    from {{ source('lakehouse_bronze', 'gekentekendevoertuigen') }}
+
+        max(afwijkende_maximum_snelheid) as afwijkende_maximum_snelheid
+    from base
+    group by
+        aantal_cilinders,
+        cilinderinhoud,
+        type_gasinstallatie,
+        zuinigheidsclassificatie,
+        vermogen_massarijklaar
 )
 
-select * from enriched;
+select * from dedup;
